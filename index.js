79812469b6a008ed713bf03062f3d1f7b1766785
ansariu007 <ansariumair250@gmail.com>
2024-10-13 15:18:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+rightnav = document.querySelector('.rightnav')
+navlist = document.querySelector('.nav-list')
+
+
+burger.addEventListener('click', ()=>{
+    rightnav.classList.toggle('v-class');
+    navlist.classList.toggle('v-class');
+    navbar.classList.toggle('h-nav');
+
+})
